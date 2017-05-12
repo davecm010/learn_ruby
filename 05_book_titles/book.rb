273bd4910caa0136d61d36dd897762ea1eab86c9
David Moylan <davecm010@gmail.com>
2017-05-12 13:41:03 -0500

    All files work

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,7 @@
 class Book
-# write your code here
+  attr_accessor :title
+  def title
+    stop_words = %w{a an and the or for of nor in}
+    @title.split.each_with_index.map{|word, index| stop_words.include?(word) && index > 0 ? word : word.capitalize }.join(" ")
+  end
 end
